923ec9c29cf171047a2a993defda8d5d2260356e
Risae <37260321+Risae@users.noreply.github.com>
2022-08-18 07:29:48 +0200

    Update GL-Toolkit-Windows.bat

--- GL-Toolkit/GL-Toolkit-Windows.bat
+++ GL-Toolkit/GL-Toolkit-Windows.bat
@@ -1,20 +1,22 @@
 @ECHO OFF
+::Set the size of the cmd window and the title of it
 mode 105,25
+title GL-Toolkit
 
-::Set current path as variable
+::Set the current folder path as a variable
 for %%i in ("%~dp0..") do set "FOLDERPATH=%%~fi"
 
-::Set Python embed into Path
+::Place the Python embed folder path into PATH
 SET "PATH=%PATH%;%FOLDERPATH%\GL-Toolkit\00_3rd_Party_Programs\python-3.10.6-embed-amd64"
 
-title GL-Toolkit
+::Check if the file GL-Toolkit.py exists - If it doesn't exist: tell the user
 IF EXIST "%~dp0GL-Toolkit.py" (
 	color 00
 	python GL-Toolkit.py
 ) ELSE (
 	color 04
-	echo. Script wasn't found! Closing in 10 seconds!
-	timeout 10 /nobreak > nul
+	echo. Script wasn't found! Closing in 15 seconds!
+	timeout 15 /nobreak > nul
 	exit
 )
 
