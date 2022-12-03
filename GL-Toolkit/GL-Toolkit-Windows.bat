b6fde97df84df907aac3f7f6d4d2fbbfe952a2f8
Risae <slayer@sajya.de>
2022-12-03 18:17:55 +0100

    Update GL-Toolkit-Windows.bat

--- GL-Toolkit/GL-Toolkit-Windows.bat
+++ GL-Toolkit/GL-Toolkit-Windows.bat
@@ -1,26 +1,41 @@
 @ECHO OFF
-::change CHCP to UTF-8
+
+:: Set the code page to UTF-8
 CHCP 65001
 
-::Set the size of the cmd window and the title of it
+:: Set the size and title of the command prompt window
 mode 105,25
 title GL-Toolkit
 
-::Set the current folder path as a variable
-for %%i in ("%~dp0..") do set "FOLDERPATH=%%~fi"
+:: Set the current folder path as a variable
+for %%i in ("%~dp0..") do set "ROOT_FOLDER=%%~fi"
 
-::Place the Python embed folder path into PATH
-SET "PATH=%PATH%;%FOLDERPATH%\GL-Toolkit\00_3rd_Party_Programs\python"
+:: Place the Python embed folder path into PATH
+SET "PATH=%PATH%;%ROOT_FOLDER%\GL-Toolkit\00_3rd_Party_Programs\python"
 
-::Check if the file "GL-Toolkit.py" exists - If it doesn't: tell the user
+:: Check if the GL-Toolkit script exists
 IF EXIST "%~dp0GL-Toolkit.py" (
-	color 00
-	python GL-Toolkit.py
+
+    :: Set the console color to green
+    color 0A
+
+    :: Run the GL-Toolkit script
+    python GL-Toolkit.py
+
 ) ELSE (
-	color 04
-	echo. Script wasn't found! Closing in 15 seconds!
-	timeout 15 /nobreak > nul
-	exit
+
+    :: Set the console color to red
+    color 04
+
+    :: Display an error message
+    echo. Script wasn't found! Closing in 15 seconds!
+
+    :: Wait for 15 seconds before closing the command prompt window
+    timeout 15 /nobreak > nul
+
+    :: Exit the script
+    exit
 )
 
+:: Wait for user input before closing the command prompt window
 pause
